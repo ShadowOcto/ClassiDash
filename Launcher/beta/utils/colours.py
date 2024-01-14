89466f516afcd3853a84fc015228287b8748ccf0
Shadow <80975779+ShadowOcto@users.noreply.github.com>
2024-01-14 10:38:26 +0000

    Create colours.py

---
+++ Launcher/beta/utils/colours.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Style
+
+red = Fore.RED
+green = Fore.GREEN
+purple = Fore.MAGENTA
+white = Fore.WHITE
+yellow = Fore.YELLOW
+cyan = Fore.CYAN
+blue = Fore.CYAN + Style.BRIGHT
+reset = Fore.RESET
